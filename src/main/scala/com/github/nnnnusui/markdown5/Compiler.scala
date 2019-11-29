3096fa6506818c2b142cefd9e1812a78d7604061
nnnnusui <nnnnusui@gmail.com>
2019-11-30 03:32:37 +0900

    📝 Sample.scala moved to test directory

--- src/main/scala/com/github/nnnnusui/markdown5/Compiler.scala
+++ src/main/scala/com/github/nnnnusui/markdown5/Compiler.scala
@@ -1,8 +1,5 @@
 package com.github.nnnnusui.markdown5
 
-/* reference:
- *   https://enear.github.io/2016/03/31/parser-combinators/
- */
 object Compiler {
   def apply(text: String): Either[CompilationError, Markdown5] ={
     for {
