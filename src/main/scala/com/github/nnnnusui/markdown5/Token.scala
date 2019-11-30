e7781a121b932faa3863336881a358c973799391
nnnnusui <nnnnusui@gmail.com>
2019-11-30 12:07:19 +0900

    📝➕ Token.{Indent, Dedent}

--- src/main/scala/com/github/nnnnusui/markdown5/Token.scala
+++ src/main/scala/com/github/nnnnusui/markdown5/Token.scala
@@ -4,6 +4,8 @@ sealed trait Token
 object Token {
   case class Indentation(depth: Int) extends Token
   case class Text(value: String) extends Token
+  case object Indent extends Token
+  case object Dedent extends Token
 
   case class CodeBlockEnclosure(attributesText: String) extends Token
 }
