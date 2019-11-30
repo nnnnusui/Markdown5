c0370f124b9aabdae127b052f0dbc439e058e7c8
nnnnusui <nnnnusui@gmail.com>
2019-11-30 11:55:14 +0900

    📝➕ Token.CodeBlockEnclosure

--- src/main/scala/com/github/nnnnusui/markdown5/Token.scala
+++ src/main/scala/com/github/nnnnusui/markdown5/Token.scala
@@ -4,4 +4,6 @@ sealed trait Token
 object Token {
   case class Indent(depth: Int) extends Token
   case class Text(value: String) extends Token
+
+  case class CodeBlockEnclosure(attributesText: String) extends Token
 }
