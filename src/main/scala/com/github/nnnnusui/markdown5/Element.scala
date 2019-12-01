b82e5ba525cb208e2eafb3d411a149236f384038
nnnnusui <nnnnusui@gmail.com>
2019-12-01 15:17:17 +0900

    📝➕ Element.{Title, CodeBlock}

--- src/main/scala/com/github/nnnnusui/markdown5/Element.scala
+++ src/main/scala/com/github/nnnnusui/markdown5/Element.scala
@@ -2,7 +2,9 @@ package com.github.nnnnusui.markdown5
 
 sealed trait Element
 object Element{
+  case class Title(text: String) extends Element
   case class Line(value: String) extends Element
   case class Paragraph(lines: List[Line]) extends Element
-  case class Block(contents: Element) extends Element
+  case class Block(contents: List[Element]) extends Element
+  case class CodeBlock(contents: List[Element]) extends Element
 }
