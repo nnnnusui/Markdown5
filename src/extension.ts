5e0466a964d5a18ad0d9f53b41f73b2679abc5d3
nnnnusui <nnnnusui@gmail.com>
2021-02-22 15:15:48 +0900

    add: outline

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const id = "markdown5";
 export function activate(context: vscode.ExtensionContext) {
+	const symbol = vscode.languages.registerDocumentSymbolProvider([
+		{ language: id },
+	], new SymbolProvider());
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown5" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown5.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Markdown5!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(symbol);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+class SymbolProvider implements vscode.DocumentSymbolProvider {
+
+    public provideDocumentSymbols(
+        document: vscode.TextDocument,
+        token: vscode.CancellationToken): Thenable<vscode.SymbolInformation[]> {
+		return new Promise((resolve, reject) => resolve(
+			[...Array(document.lineCount).keys()]
+					.map((_, index)=> index)
+					.map(index => document.lineAt(index))
+					.map(line => {
+						const match = this.pattern.exec(line.text);
+						if (!match) {return null;}
+						return {
+							name: match[2],
+							kind: vscode.SymbolKind.Class,
+							location: new vscode.Location(document.uri, line.range)
+						};
+					})
+					.filter((it): it is vscode.SymbolInformation => it !== null)
+		));
+    }
+
+    private get pattern() {
+        return /^(#) (.*)/gm;
+    }
+}
