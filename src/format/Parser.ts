ba4e2b3d362c72b9aa5b4c1a5da081b8186fc80f
nnnnusui <nnnnusui@gmail.com>
2021-03-02 23:54:05 +0900

    add: TokenKind

--- src/format/Parser.ts
+++ src/format/Parser.ts
@@ -1,4 +1,50 @@
+
+const enum TokenKind {
+  Indent,
+  Section,
+  Header,
+  Text,
+}
+type Indent = {
+  kind: TokenKind.Indent,
+  value: String,
+}
+type Text = {
+  kind: TokenKind.Text,
+  value: String,
+}
+type Header = {
+  kind: TokenKind.Header,
+  text: String,
+}
+type Section = {
+  kind: TokenKind.Section,
+  header: Header,
+  child: Content[],
+}
+type Content = Section | Text
+
+type Result<Err, Ok> = {
+  ok: true,
+  get: Ok,
+} | {
+  ok: false,
+  get: Err,
+}
+
 const Parser = () => {
+  function ok<T>(it: T) {
+    return {
+      ok: true,
+      get: it,
+    } as const
+  }
+  function err<T>(it: T) {
+    return {
+      ok: false,
+      get: it,
+    } as const
+  }
   function parse(lines: string[]) {
     const section = parseSection(lines);
     return { section };
@@ -15,11 +61,17 @@ const Parser = () => {
   }
   function parseSection(lines: string[]) {
     const [head, ...tails] = dropEmptyLines(lines);
-    const indent = parseIndent(head);
-    const header = parseHeader(head.slice(indent.length));
-    if (!header) {
+    const indentRes = parseIndent(head);
+    if (!indentRes.ok) {
       return;
     }
+    const indent = indentRes.get
+    const indentLength = indent.value.length
+    const headerRes = parseHeader(head.slice(indentLength));
+    if (!headerRes.ok) {
+      return;
+    }
+    const header = headerRes.get
 
     function getContentLines(
       lines: string[],
@@ -27,7 +79,7 @@ const Parser = () => {
     ): [string[], string[]] {
       const [head, ...tails] = lines;
       const isNotEmptyLine = head.trim() !== "";
-      const dedented = head.slice(0, indent.length) !== indent;
+      const dedented = head.slice(0, indentLength) !== indent.value;
       if (isNotEmptyLine && dedented) {
         return [result, lines];
       }
@@ -36,22 +88,22 @@ const Parser = () => {
     const [contentLines, afters] = getContentLines(tails);
     return {
       header,
-      content: contentLines.map((it) => it.slice(indent.length)),
+      content: contentLines.map((it) => it.slice(indentLength)),
     };
   }
-  function parseHeader(line: string) {
+  function parseHeader(line: string): Result<string, Header> {
     const prefix = "# ";
     if (!line.startsWith(prefix)) {
-      return;
+      return err("failure on parseHeader()");
     }
     const content = line.slice(prefix.length);
-    return { content };
+    return ok({kind: TokenKind.Header, text: content});
   }
-  function parseIndent(line: string) {
+  function parseIndent(line: string): Result<string, Indent> {
     const trimed = line.trimStart();
     const depth = line.length - trimed.length;
     const indent = line.slice(0, depth);
-    return indent;
+    return ok({kind: TokenKind.Indent, value: indent});
   }
   return { parse };
 };
