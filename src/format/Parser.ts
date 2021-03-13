ce20d1a4724010c683e3d2ae5e75bdfe9f31105b
nnnnusui <nnnnusui@gmail.com>
2021-03-13 22:14:14 +0900

    add: type char

--- src/format/Parser.ts
+++ src/format/Parser.ts
@@ -1,12 +1,26 @@
 import { convert, repeat, not, chain, same, or } from "../parser/Combinators";
 import { Parser } from "../parser/Types";
 
+type Char = string & { length: 1 };
+declare global {
+  interface String {
+    char(): Char;
+    chars(): Char[];
+  }
+}
+String.prototype[`char`] = function () {
+  return this.charAt(0) as Char;
+};
+String.prototype[`chars`] = function () {
+  return this.split("") as Char[];
+};
+
 const to = <T, Src>(parser: Parser<T, Src>): Parser<Src[], Src> => {
   const content = repeat(not(parser));
   const hasTail = convert(chain(content, parser), ([content]) => content);
   return or(hasTail, content);
 };
-const eol = same("\n");
+const eol = same("\n".char());
 const line = to(eol);
 
-export const parse = (src: string) => line(src.split(""));
+export const parse = (src: string) => line(src.chars());
