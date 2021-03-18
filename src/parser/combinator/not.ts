fdfd9f5e932c4fc32d0864e4a27e9d8baca297c5
nnnnusui <nnnnusui@gmail.com>
2021-03-19 02:30:41 +0900

    fix: not parser to not read

--- src/parser/combinator/not.ts
+++ src/parser/combinator/not.ts
@@ -1,9 +1,9 @@
 import { Parser } from "../Types";
 
-const not = <T, Src>(parser: Parser<T, Src>): Parser<T, Src> => (
+const not = <T, Src>(parser: Parser<T, Src>): Parser<null, Src> => (
   src: Src[]
 ) => {
-  const { ok, ...results } = parser(src);
-  return { ok: !ok, ...results };
+  const { ok } = parser(src);
+  return { ok: !ok, head: null, tails: src };
 };
 export default not;
