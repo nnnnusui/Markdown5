a316f3165c3f70710eac56798919b2a0ce1d24e1
nnnnusui <nnnnusui@gmail.com>
2021-03-23 05:26:47 +0900

    update: export function on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import { parse } from "./format/Parser";
-import { transpile } from "./format/Transpiler";
+export { parse } from "./format/Parser";
+export { transpile } from "./format/Transpiler";
 
-const text = `
-# Markdown5
-# paragraph
-  # empty line separating
-  sample paragraph
+// const text = `
+// # Markdown5
+// # paragraph
+//   # empty line separating
+//   sample paragraph
 
-  double line
-  paragraph
+//   double line
+//   paragraph
 
-  # paragraph-indent separating
-   first paragra-
-  ph. sample text.
-   second paragraph
-   third paragarph sample
-  text.
-`;
-const parsed = parse(text);
-console.dir(parsed, { depth: null });
-const transpiled = transpile(parsed.head);
-console.dir(transpiled, { depth: null });
+//   # paragraph-indent separating
+//    first paragra-
+//   ph. sample text.
+//    second paragraph
+//    third paragarph sample
+//   text.
+// `;
+// const parsed = parse(text);
+// console.dir(parsed, { depth: null });
+// const transpiled = transpile(parsed.head);
+// console.dir(transpiled, { depth: null });
