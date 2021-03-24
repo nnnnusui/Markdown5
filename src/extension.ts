c6c75e7421e792ca017bcc472c6d6d104e456e02
nnnnusui <nnnnusui@gmail.com>
2021-03-25 00:41:36 +0900

    update: symbol generate from parsed tokens

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from "vscode";
+import Markdown5 from "markdown5";
+import { Token, TokenKind, TokenValue } from "markdown5/dist/format/Types";
 
 const id = "markdown5";
 export function activate(context: vscode.ExtensionContext) {
@@ -19,26 +21,61 @@ class SymbolProvider implements vscode.DocumentSymbolProvider {
   ): Thenable<vscode.SymbolInformation[]> {
     return new Promise((resolve, reject) =>
       resolve(
-        [...Array(document.lineCount).keys()]
-          .map((_, index) => index)
-          .map((index) => document.lineAt(index))
-          .map((line) => {
-            const match = this.pattern.exec(line.text);
-            if (!match) {
-              return null;
-            }
-            return {
-              name: match[2],
-              kind: vscode.SymbolKind.Class,
-              location: new vscode.Location(document.uri, line.range),
-            };
-          })
-          .filter((it): it is vscode.SymbolInformation => it !== null)
+        Markdown5.parse(document.getText()).flatMap((it) =>
+          this.symbolFromToken(it, document)
+        )
       )
     );
   }
-
-  private get pattern() {
-    return /^(#) (.*)/gm;
+  private symbolFromToken<T extends TokenKind>(
+    token: Token<T>,
+    document: vscode.TextDocument,
+    parent = ""
+  ) {
+    const position = document.positionAt(token.offset);
+    return this.searchSymbol(
+      token,
+      new vscode.Location(document.uri, position),
+      document,
+      parent
+    );
+  }
+  private searchSymbol(
+    token: TokenValue,
+    location: vscode.Location,
+    document: vscode.TextDocument,
+    parent: string
+  ): vscode.SymbolInformation[] {
+    switch (token.kind) {
+      default:
+        return [];
+      case "markdown5": {
+        const { title, contents } = token.value;
+        return [
+          ...this.symbolFromToken(title, document, parent),
+          ...contents.flatMap((it) =>
+            this.symbolFromToken(it, document, title.value)
+          ),
+        ];
+      }
+      case "section": {
+        const { header, contents } = token.value;
+        return [
+          ...this.symbolFromToken(header, document, parent),
+          ...contents.flatMap((it) =>
+            this.symbolFromToken(it, document, header.value)
+          ),
+        ];
+      }
+      case "sectionHeader":
+        return [
+          {
+            containerName: parent,
+            name: token.value,
+            kind: vscode.SymbolKind.Key,
+            location,
+          },
+        ];
+    }
   }
 }
