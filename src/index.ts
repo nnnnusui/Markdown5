a03f6934ff31437732f99608bd5b6d91aa714efd
nnnnusui <nnnnusui@gmail.com>
2021-04-07 23:46:05 +0900

    add: inject ogp info

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { glob } from "glob";
 import { readFileSync, stat } from "fs";
 import { parse } from "./format/Parser";
 import { transpile } from "./format/Transpiler";
+import { Token } from "./format/Types";
 
 const Markdown5 = {
   parse: (source: string) => {
@@ -16,6 +17,21 @@ const Markdown5 = {
 export default Markdown5;
 export * from "./format/Types";
 
+const Template = {
+  getHead: (m5: Token<"markdown5">) => {
+    const title = m5.value.title.value;
+    const firstContent = m5.value.contents[0];
+    const description =
+      firstContent.kind === "paragraph" ? firstContent.value : "...";
+    const result = `
+      <meta property="og:title" content="${title}" />
+      <meta property="og:description" content="${description}" />
+      <title>${title}</title>
+    `;
+    return result;
+  },
+};
+
 const cli = cac();
 cli.command("compile <path>", "transpile to html").action((path: string) => {
   stat(path, (err, stats) => {
@@ -27,12 +43,13 @@ cli.command("compile <path>", "transpile to html").action((path: string) => {
       glob(pathPattern, (err, paths) => {
         const transpiledResults = paths.flatMap((path) => {
           const text = readFileSync(path, { encoding: "utf8" });
-          return Markdown5.parse(text).map(
-            (it) => [it.value.title.value, Markdown5.transpile(it)] as const
-          );
+          return Markdown5.parse(text).map((it) => ({
+            body: Markdown5.transpile(it),
+            head: Template.getHead(it),
+          }));
         });
-        const templateApplyed = transpiledResults.map(([title, html]) =>
-          template.replace("{title}", title).replace("{body}", html)
+        const templateApplyed = transpiledResults.map(({ head, body }) =>
+          template.replace("{head}", head).replace("{body}", body)
         );
         console.log(templateApplyed);
       });
