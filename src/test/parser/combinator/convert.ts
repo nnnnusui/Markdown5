0e93ea2b9fdaa90d28ffa657362d8beda159dec7
nnnnusui <nnnnusui@gmail.com>
2021-04-09 16:10:26 +0900

    update: test convert

--- src/test/parser/combinator/convert.ts
+++ src/test/parser/combinator/convert.ts
@@ -2,15 +2,17 @@ import { expect } from "chai";
 import convert from "../../../parser/combinator/convert";
 import same from "../../../parser/combinator/minimum/same";
 import { init } from "../../../parser/combinator/util/init";
+import { ok } from "../../../parser/Types";
 
 it("convert test", () => {
   const a = convert(same(1), (it) => `${it}`);
-  expect(init(a)([1, 2])).to.deep.equal({
-    ok: true,
-    head: "1",
-    tails: {
-      offset: 1,
-      values: [2],
-    },
-  });
+  expect(init(a)([1, 2])).to.deep.equal(
+    ok({
+      head: "1",
+      tail: {
+        offset: 1,
+        values: [2],
+      },
+    })
+  );
 });
