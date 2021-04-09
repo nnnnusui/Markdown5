4a2332bc454159bf424b4fecc269735fe7ac733c
nnnnusui <nnnnusui@gmail.com>
2021-04-09 13:12:19 +0900

    update: same

--- src/parser/combinator/minimum/same.ts
+++ src/parser/combinator/minimum/same.ts
@@ -1,8 +1,11 @@
-import { Parser } from "../../Types";
+import { Combinator, err } from "../../Types";
 import any from "./any";
 
-const same = <Src>(it: Src): Parser<Src, Src> => (src) => {
-  const { head, tails } = any<Src>()(src);
-  return { ok: it === head, head, tails };
+const same = <Src>(it: Src): Combinator<Src, Src> => (src) => {
+  const read = any<Src>()(src);
+  if(!read.ok) return read;
+  const head = read.get.head;
+  if (head !== it) return err(src);
+  return read;
 };
 export default same;
