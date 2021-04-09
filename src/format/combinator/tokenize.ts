e71ca1b64e2357ff5eb4e9646e0bb612bbcb7114
nnnnusui <nnnnusui@gmail.com>
2021-04-09 17:35:33 +0900

    update: tokenize export default

--- src/format/combinator/tokenize.ts
+++ src/format/combinator/tokenize.ts
@@ -2,8 +2,9 @@ import convert from "../../parser/combinator/convert";
 import { Combinator } from "../../parser/Types";
 import { TokenKind, FindFromUnion, TokenValue, Token } from "../Types";
 
-export const tokenize = <Src, Before, After extends TokenKind>(
+const tokenize = <Src, Before, After extends TokenKind>(
   combinator: Combinator<Before, Src>,
   func: (before: Before) => FindFromUnion<TokenValue, "kind", After>
 ): Combinator<Token<After>, Src> =>
   convert(combinator, (it, offset) => Token(func(it), offset));
+export default tokenize;
