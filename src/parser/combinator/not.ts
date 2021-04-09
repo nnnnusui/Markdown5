9ef76017194745f5e0ff3efd640dcbcf3a2af3bd
nnnnusui <nnnnusui@gmail.com>
2021-04-09 13:26:33 +0900

    update: not

--- src/parser/combinator/not.ts
+++ src/parser/combinator/not.ts
@@ -1,7 +1,6 @@
-import { Parser } from "../Types";
+import { Combinator, err, ok } from "../Types";
 
-const not = <T, Src>(parser: Parser<T, Src>): Parser<null, Src> => (src) => {
-  const { ok } = parser(src);
-  return { ok: !ok, head: null, tails: src };
-};
+const not = <T, Src>(combinator: Combinator<T, Src>): Combinator<null, Src> => (
+  src
+) => (combinator(src).ok ? err(src) : ok({ head: null, tail: src }));
 export default not;
