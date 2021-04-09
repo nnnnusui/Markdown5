9a49e49b7881a5ddab328e98c8afc0e62a980813
nnnnusui <nnnnusui@gmail.com>
2021-04-09 15:48:51 +0900

    update: test same

--- src/test/parser/combinator/minimum/same.ts
+++ src/test/parser/combinator/minimum/same.ts
@@ -1,23 +1,23 @@
 import { expect } from "chai";
 import same from "../../../../parser/combinator/minimum/same";
 import { init } from "../../../../parser/combinator/util/init";
+import { err, ok } from "../../../../parser/Types";
 
 it("same test", () => {
   const a = same("a");
-  expect(init(a)("ab".split(""))).to.deep.equal({
-    ok: true,
-    head: "a",
-    tails: {
-      offset: 1,
-      values: ["b"],
-    },
-  });
-  expect(init(a)("bb".split(""))).to.deep.equal({
-    ok: false,
-    head: "b",
-    tails: {
-      offset: 1,
-      values: ["b"],
-    },
-  });
+  expect(init(a)("ab".split(""))).to.deep.equal(
+    ok({
+      head: "a",
+      tail: {
+        offset: 1,
+        values: ["b"],
+      },
+    })
+  );
+  expect(init(a)("bb".split(""))).to.deep.equal(
+    err({
+      offset: 0,
+      values: ["b", "b"],
+    })
+  );
 });
