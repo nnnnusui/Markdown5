66fb0f91449b263a2cc9a227b6257c213b279a80
nnnnusui <nnnnusui@gmail.com>
2021-04-09 13:26:25 +0900

    update: same

--- src/parser/combinator/minimum/same.ts
+++ src/parser/combinator/minimum/same.ts
@@ -3,7 +3,7 @@ import any from "./any";
 
 const same = <Src>(it: Src): Combinator<Src, Src> => (src) => {
   const read = any<Src>()(src);
-  if(!read.ok) return read;
+  if (!read.ok) return read;
   const head = read.get.head;
   if (head !== it) return err(src);
   return read;
