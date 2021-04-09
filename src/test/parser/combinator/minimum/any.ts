c2af2f5bc5258e292caffe178f151d880d7fc6dc
nnnnusui <nnnnusui@gmail.com>
2021-04-09 15:48:36 +0900

    update: test any

--- src/test/parser/combinator/minimum/any.ts
+++ src/test/parser/combinator/minimum/any.ts
@@ -1,24 +1,27 @@
 import { expect } from "chai";
 import any from "../../../../parser/combinator/minimum/any";
 import { init } from "../../../../parser/combinator/util/init";
+import { ok } from "../../../../parser/Types";
 
 it("any test", () => {
   const a = any<string>();
 
-  expect(init(a)("a".split(""))).to.deep.equal({
-    ok: true,
-    head: "a",
-    tails: {
-      offset: 1,
-      values: [],
-    },
-  });
-  expect(init(a)("bc".split(""))).to.deep.equal({
-    ok: true,
-    head: "b",
-    tails: {
-      offset: 1,
-      values: ["c"],
-    },
-  });
+  expect(init(a)("a".split(""))).to.deep.equal(
+    ok({
+      head: "a",
+      tail: {
+        offset: 1,
+        values: [],
+      },
+    })
+  );
+  expect(init(a)("bc".split(""))).to.deep.equal(
+    ok({
+      head: "b",
+      tail: {
+        offset: 1,
+        values: ["c"],
+      },
+    })
+  );
 });
