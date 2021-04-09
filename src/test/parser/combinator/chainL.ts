57285ad99871c80ba7794543524b85bdd1e67c69
nnnnusui <nnnnusui@gmail.com>
2021-04-09 16:09:27 +0900

    update: test chainL

--- src/test/parser/combinator/chainL.ts
+++ src/test/parser/combinator/chainL.ts
@@ -2,15 +2,17 @@ import { expect } from "chai";
 import chainL from "../../../parser/combinator/chainL";
 import same from "../../../parser/combinator/minimum/same";
 import { init } from "../../../parser/combinator/util/init";
+import { ok } from "../../../parser/Types";
 
 it("chainL test", () => {
   const a = chainL(same("a"), same("b"));
-  expect(init(a)("aba".split(""))).to.deep.equal({
-    ok: true,
-    head: "a",
-    tails: {
-      offset: 2,
-      values: ["a"],
-    },
-  });
+  expect(init(a)("aba".split(""))).to.deep.equal(
+    ok({
+      head: "a",
+      tail: {
+        offset: 2,
+        values: ["a"],
+      },
+    })
+  );
 });
