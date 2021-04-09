60ed6b892e20197770eea242ce64e35eda68635d
nnnnusui <nnnnusui@gmail.com>
2021-04-09 16:09:33 +0900

    update: test chainR

--- src/test/parser/combinator/chainR.ts
+++ src/test/parser/combinator/chainR.ts
@@ -2,15 +2,17 @@ import { expect } from "chai";
 import chainR from "../../../parser/combinator/chainR";
 import same from "../../../parser/combinator/minimum/same";
 import { init } from "../../../parser/combinator/util/init";
+import { ok } from "../../../parser/Types";
 
 it("chainR test", () => {
   const a = chainR(same("a"), same("b"));
-  expect(init(a)("aba".split(""))).to.deep.equal({
-    ok: true,
-    head: "b",
-    tails: {
-      offset: 2,
-      values: ["a"],
-    },
-  });
+  expect(init(a)("aba".split(""))).to.deep.equal(
+    ok({
+      head: "b",
+      tail: {
+        offset: 2,
+        values: ["a"],
+      },
+    })
+  );
 });
