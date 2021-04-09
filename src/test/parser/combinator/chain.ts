c112c55162b4d5a25cc629fa4da5eb740f0f0eb0
nnnnusui <nnnnusui@gmail.com>
2021-04-09 16:08:18 +0900

    update: test chain

--- src/test/parser/combinator/chain.ts
+++ src/test/parser/combinator/chain.ts
@@ -3,33 +3,34 @@ import chain from "../../../parser/combinator/chain";
 import convert from "../../../parser/combinator/convert";
 import same from "../../../parser/combinator/minimum/same";
 import { init } from "../../../parser/combinator/util/init";
+import { err, ok } from "../../../parser/Types";
 
 it("chain test", () => {
   const a = chain(same("a"), same("b"));
-  expect(init(a)("aba".split(""))).to.deep.equal({
-    ok: true,
-    head: ["a", "b"],
-    tails: {
-      offset: 2,
-      values: ["a"],
-    },
-  });
-  expect(init(a)("aaba".split(""))).to.deep.equal({
-    ok: false,
-    head: ["a"],
-    tails: {
+  expect(init(a)("aba".split(""))).to.deep.equal(
+    ok({
+      head: ["a", "b"],
+      tail: {
+        offset: 2,
+        values: ["a"],
+      },
+    })
+  );
+  expect(init(a)("aaba".split(""))).to.deep.equal(
+    err({
       offset: 1,
       values: ["a", "b", "a"],
-    },
-  });
+    })
+  );
   const x = convert(same("x"), (it) => ({ v: it }));
   const c = chain(a, x);
-  expect(init(c)("abxc".split(""))).to.deep.equal({
-    ok: true,
-    head: [["a", "b"], { v: "x" }],
-    tails: {
-      offset: 3,
-      values: ["c"],
-    },
-  });
+  expect(init(c)("abxc".split(""))).to.deep.equal(
+    ok({
+      head: [["a", "b"], { v: "x" }],
+      tail: {
+        offset: 3,
+        values: ["c"],
+      },
+    })
+  );
 });
