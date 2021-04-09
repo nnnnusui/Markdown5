ea62351a765e24ef7ee6c20cafc559209a872511
nnnnusui <nnnnusui@gmail.com>
2021-04-09 14:44:04 +0900

    update: or

--- src/parser/combinator/or.ts
+++ src/parser/combinator/or.ts
@@ -1,15 +1,16 @@
-import { Parsers, Parser, UnifiedParsersResult, ParsersSrc } from "../Types";
+import { AnyCombinators, Combinator, err, Src, UnifiedHead } from "../Types";
 
-const or = <T extends Parsers<any>>(
-  ...parsers: T
-): Parser<UnifiedParsersResult<typeof parsers>, ParsersSrc<T>> => (src) => {
-  const recursion = (index: number): any => {
-    // power
-    if (parsers.length <= index) return { ok: false, head: [], tails: src };
-    const result = parsers[index](src);
-    if (result.ok) return result;
-    return recursion(index + 1);
-  };
-  return recursion(0);
+const or = <T extends AnyCombinators>(
+  ...combinators: T
+): Combinator<UnifiedHead<T>, Src<T>> => (src) => {
+  return combinators.reduce<ReturnType<Combinator<UnifiedHead<T>, Src<T>>>>(
+    (result, it) => {
+      if (result.ok) return result;
+      const current = it(src);
+      if (!current.ok) return result;
+      return current;
+    },
+    err(src)
+  );
 };
 export default or;
