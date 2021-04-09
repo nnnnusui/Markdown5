1299f8014657378862c462153036ec913cd8f6c3
nnnnusui <nnnnusui@gmail.com>
2021-04-09 16:12:56 +0900

    update: test option

--- src/test/parser/combinator/option.ts
+++ src/test/parser/combinator/option.ts
@@ -2,9 +2,26 @@ import { expect } from "chai";
 import same from "../../../parser/combinator/minimum/same";
 import option from "../../../parser/combinator/option";
 import { init } from "../../../parser/combinator/util/init";
+import { ok } from "../../../parser/Types";
 
 it("option test", () => {
   const a = option(same("a"));
-  expect(init(a)("abb".split("")).head).to.deep.equal("a");
-  expect(init(a)("bb".split("")).head).to.deep.equal(null);
+  expect(init(a)("abb".split(""))).to.deep.equal(
+    ok({
+      head: "a",
+      tail: {
+        offset: 1,
+        values: ["b", "b"],
+      },
+    })
+  );
+  expect(init(a)("bb".split(""))).to.deep.equal(
+    ok({
+      head: null,
+      tail: {
+        offset: 0,
+        values: ["b", "b"],
+      },
+    })
+  );
 });
