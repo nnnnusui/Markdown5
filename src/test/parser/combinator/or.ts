67ef35225c43f452b3b174fd180f5541c55e4bd4
nnnnusui <nnnnusui@gmail.com>
2021-04-09 16:11:40 +0900

    update: test or

--- src/test/parser/combinator/or.ts
+++ src/test/parser/combinator/or.ts
@@ -4,9 +4,18 @@ import same from "../../../parser/combinator/minimum/same";
 import or from "../../../parser/combinator/or";
 import repeat from "../../../parser/combinator/repeat";
 import { init } from "../../../parser/combinator/util/init";
+import { ok } from "../../../parser/Types";
 
 it("or test", () => {
   const x = convert(same("x"), (it) => ({ v: it }));
   const a = repeat(or(same("a"), x));
-  expect(init(a)("axab".split("")).head).to.deep.equal(["a", { v: "x" }, "a"]);
+  expect(init(a)("axab".split(""))).to.deep.equal(
+    ok({
+      head: ["a", { v: "x" }, "a"],
+      tail: {
+        offset: 3,
+        values: ["b"],
+      },
+    })
+  );
 });
