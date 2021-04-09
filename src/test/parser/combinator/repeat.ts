1eea1e4be2494c3522e35a84f7e7f0ba602f3f20
nnnnusui <nnnnusui@gmail.com>
2021-04-09 16:14:15 +0900

    update: test repeat

--- src/test/parser/combinator/repeat.ts
+++ src/test/parser/combinator/repeat.ts
@@ -2,9 +2,26 @@ import { expect } from "chai";
 import same from "../../../parser/combinator/minimum/same";
 import repeat from "../../../parser/combinator/repeat";
 import { init } from "../../../parser/combinator/util/init";
+import { ok } from "../../../parser/Types";
 
 it("repeat test", () => {
   const a = repeat(same("a"));
-  expect(init(a)("aaaaxx".split("")).head).to.deep.equal(["a", "a", "a", "a"]);
-  expect(init(a)("xax".split("")).head).to.deep.equal([]);
+  expect(init(a)("aaaaxx".split(""))).to.deep.equal(
+    ok({
+      head: ["a", "a", "a", "a"],
+      tail: {
+        offset: 4,
+        values: ["x", "x"],
+      },
+    })
+  );
+  expect(init(a)("xax".split(""))).to.deep.equal(
+    ok({
+      head: [],
+      tail: {
+        offset: 0,
+        values: ["x", "a", "x"],
+      },
+    })
+  );
 });
