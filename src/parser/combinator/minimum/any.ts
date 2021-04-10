e0b48537e464c584c1c4f747c9d774044b4b02b6
nnnnusui <nnnnusui@gmail.com>
2021-04-10 11:05:00 +0900

    update: any don't match empty string

--- src/parser/combinator/minimum/any.ts
+++ src/parser/combinator/minimum/any.ts
@@ -1,10 +1,11 @@
-import { Combinator, ok } from "../../Types";
+import { Combinator, err, ok } from "../../Types";
 
 const any = <Src>(): Combinator<Src, Src> => (src) => {
   const {
     values: [head, ...tails],
     offset,
   } = src;
+  if (!head) return err(src);
   return ok({ head, tail: { values: tails, offset: offset + 1 } });
 };
 export default any;
