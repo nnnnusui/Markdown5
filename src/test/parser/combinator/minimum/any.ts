d7844a35ebeb19f3f17a148c0f8fa12a716bd30b
nnnnusui <nnnnusui@gmail.com>
2021-04-10 11:06:27 +0900

    update: devide any tests

--- src/test/parser/combinator/minimum/any.ts
+++ src/test/parser/combinator/minimum/any.ts
@@ -1,27 +1,38 @@
 import { expect } from "chai";
 import any from "../../../../parser/combinator/minimum/any";
 import init from "../../../../parser/combinator/util/init";
-import { ok } from "../../../../parser/Types";
+import { err, ok } from "../../../../parser/Types";
 
-it("any test", () => {
+describe("any test", () => {
   const a = any<string>();
 
-  expect(init(a)("a".split(""))).to.deep.equal(
-    ok({
-      head: "a",
-      tail: {
-        offset: 1,
+  it('any() matches "a"', () =>
+    expect(init(a)("a".chars())).to.deep.equal(
+      ok({
+        head: "a",
+        tail: {
+          offset: 1,
+          values: [],
+        },
+      })
+    ));
+
+  it('any() matches "b"', () =>
+    expect(init(a)("bc".chars())).to.deep.equal(
+      ok({
+        head: "b",
+        tail: {
+          offset: 1,
+          values: ["c"],
+        },
+      })
+    ));
+
+  it('any() does not match ""', () =>
+    expect(init(a)("".chars())).to.deep.equal(
+      err({
+        offset: 0,
         values: [],
-      },
-    })
-  );
-  expect(init(a)("bc".split(""))).to.deep.equal(
-    ok({
-      head: "b",
-      tail: {
-        offset: 1,
-        values: ["c"],
-      },
-    })
-  );
+      })
+    ));
 });
