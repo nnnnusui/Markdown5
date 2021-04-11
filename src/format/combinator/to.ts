465829d67c05331dea83b15e75a42d68cbb2a9e0
nnnnusui <nnnnusui@gmail.com>
2021-04-12 08:15:22 +0900

    refactor: rename val in to.ts

--- src/format/combinator/to.ts
+++ src/format/combinator/to.ts
@@ -8,9 +8,9 @@ import repeat from "../../parser/combinator/repeat";
 import Char from "../Char";
 import Parser from "../Parser";
 
-const to = <T>(combinator: Parser<T>, optional = true): Parser<string> => {
-  const tail = optional ? option(combinator) : combinator;
-  const content = repeat(chainR(not(combinator), any<Char>()));
+const to = <T>(parser: Parser<T>, optional = true): Parser<string> => {
+  const tail = optional ? option(parser) : parser;
+  const content = repeat(chainR(not(parser), any<Char>()));
   return convert(chainL(content, tail), (it) => it.join(""));
 };
 export default to;
